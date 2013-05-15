6ddff839518967742de5f37c8ab6a005571a0d31
leonhardt schwarz <leonhardt@neptun.sol>
2013-05-15 17:27:03 +0200

    Feature: lesen und parsen

--- LabyrinthParser.cpp
+++ LabyrinthParser.cpp
@@ -2,35 +2,36 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include <iterator>
 #include <string>
+#include <sstream>
 
 LabyrinthParser::LabyrinthParser(const char* fileName)
-	: mBuffer(std::vector<char>(0)), mWidth(0), mHeight(0)
+	: mWidth(0), mHeight(0)
 {
-	std::ifstream fileStream(fileName, std::ios::binary);
+	std::ifstream fileStream(fileName);
 	fileStream.unsetf(std::ios_base::skipws);
 
 	if(!fileStream) {
-		/* throw exception? */
+		std::cerr << "Could not open file!" << std::endl;
 	}
-	mBuffer.insert( 
-		mBuffer.begin(), 
-		std::istream_iterator<char>(fileStream), 
-		std::istream_iterator<char>());
+  	std::string lab((std::istreambuf_iterator<char>(fileStream)), 
+			std::istreambuf_iterator<char>());
 
-	/*for(int i = 0; i <= (int) mBuffer.size(); i++) {
-		int lock = 1;
-		if(mBuffer[i] == '\n') {
-			if(lock) {
-				mWidth = i;
-				lock = 0;
-			}
-			mBuffer.erase(mBuffer.begin()+i);
+	for(int i = 0; i <= lab.size(); i++) {
+		if (lab[i] == '\r')
+		{
+			mWidth = i;
+			break;
 		}
-	}*/
+	}
 
-	//mHeight = mBuffer.size() / mWidth;
+	lab.erase(std::remove(lab.begin(), lab.end(), '\n'), lab.end());
+	lab.erase(std::remove(lab.begin(), lab.end(), '\r'), lab.end());
+	std::cout << lab << std::endl;
+
+	mBuffer = std::vector<char>(lab.begin(), lab.end());
 	fileStream.close();
 }
 
