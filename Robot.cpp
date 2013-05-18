6129844380ee43d6591e3eedb18a77467fa8dbaa
leonhardt schwarz <leonhardt@neptun.sol>
2013-05-18 16:29:15 +0200

    Robot.cpp/.hpp ansätze

---
+++ Robot.cpp
@@ -0,0 +1,11 @@
+#include "Robot.hpp" 
+
+Robot::Robot(int x, int y)
+{
+	xpos = x;
+	ypos = y;
+}
+
+Robot::~Robot()
+{
+}
---
+++ Robot.hpp
@@ -0,0 +1,15 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+class Robot
+{
+	int xpos;
+	int ypos;
+public:
+	Robot (int x, int y);
+	~Robot ();
+
+private:
+	
+};
+#endif 
